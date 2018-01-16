068c2052bc9d66eb6ea9e69205f926f37f3d0589
Karen Nel <karen.nel@tyme.com>
2018-01-16 15:21:39 +0200

    Starting background functionality

--- src/background.js
+++ src/background.js
@@ -1,29 +1,65 @@
-import React, {Component} from 'react'; 
+import React, { Component } from 'react';
+import $ from 'jquery';
+import getStarted from './get-started.jpg';
 
 class Background extends Component {
     constructor() {
-        super(); 
+        super();
 
-        this.handleLoad = this.handleLoad.bind(this); 
+        this.handleLoad = this.handleLoad.bind(this);
 
+        this.state = {
+            img: {
+                width: 0
+            }
+        };
     }
 
     handleLoad() {
-        console.log('in handle load'); 
+        var background = document.getElementById('background');
+        console.log('background', background); 
+        background.innerHTML = `<img src=${getStarted} alt="background"/>`;
+
+        // console.log('image width', background.Image.width);
+        // console.log('image height', background.Image.height);
+       
+        let img = new Image();
+        let scope = this;
+
+        img.onload = function () {
+            scope.state.img.width = this.width;
+            console.log('this.width',this.width);
+            scope.resize();
+        };
+
+        img.src = getStarted;
+
+        // $('img').width(200);
+        // console.log('in handle load', app_background); 
+        //  $('.app-bg').hide;
+
     }
 
-    componentWillMount ()  {
+    resize(){
+        console.log(document.getElementsByTagName('body')[0].clientWidth);
+        console.log(document.getElementsByTagName('body')[0].clientHeight);
+    }
+
+    componentDidMount() {
         console.log('component mounted');
-        this.handleLoad()
+        this.handleLoad();
+
+        let scope = this;
+
+        window.onresize = function(event) {
+            scope.resize();
+        };
     }
 
     render() {
-
         return (
-            <div>
-                This is the content
-            </div>
-        ); 
+            <div id ='background' ></div>
+        );
     }
 }
 
